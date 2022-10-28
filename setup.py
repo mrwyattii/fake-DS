57e25591fc0b56e2b93b6981ffc1f32c6f0fcf3a
Michael Wyatt <michaelwyatt@microsoft.com>
2022-10-27 17:04:01 -0700

    add pytest to reqs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ Copyright 2022 The Microsoft DeepSpeed Team
 
 from setuptools import setup, find_packages
 
-install_requires = ["fake_mii"]
+install_requires = ["fake_mii", "pytest"]
 
 setup(name="fake_ds",
       version="1.0",
