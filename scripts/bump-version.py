b133c69b85db233a73d6e357beaf656efca131f8
Michael Wyatt <michaelwyatt@microsoft.com>
2023-05-23 15:59:37 -0700

    update release automation

---
+++ scripts/bump-version.py
@@ -0,0 +1,9 @@
+from packaging import version as pkg_version
+
+with open('../version.txt', 'r') as fd:
+        version = pkg_version.parse(fd.read())
+
+with open('./version.txt', 'w') as fd:
+    fd.write(f'{version.major}.{version.minor}.{version.micro + 1}\n')
+
+print(f'{version} -> {version.major}.{version.minor}.{version.micro + 1}')
